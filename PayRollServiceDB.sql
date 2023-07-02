63e1206aa6a8f29fafadfa261e8703630ff1c0ec
khushbu <khushbusoni475@gmail.com>
2023-07-02 13:57:53 +0530

    [khushbu] modify Create Employee Payroll Table

--- PayRollServiceDB.sql
+++ PayRollServiceDB.sql
@@ -1,3 +1,12 @@
 ---Hello World---
 ---UC1 Create PayRoll Database---
 create database payRollServiceDB
+
+---UC2 Create table----
+create table EmployeePayRoll
+(
+EmployeeId int identity(1,1) primary key,
+Name varchar(225),
+Salary float,
+StartDate date
+)
