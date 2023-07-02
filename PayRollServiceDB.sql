add2f9ef7f187362d20fb2d8f2e0c3ad6cff72d7
khushbu <khushbusoni475@gmail.com>
2023-07-02 14:20:50 +0530

    [khushbu] modify Insert Data In Employee Payroll Table

--- PayRollServiceDB.sql
+++ PayRollServiceDB.sql
@@ -10,3 +10,7 @@ Name varchar(225),
 Salary float,
 StartDate date
 )
+
+---UC3 Insert Data In Table---
+insert into EmployeePayRoll values('Twiney', 35000, getdate());
+insert into EmployeePayRoll (Name, StartDate)values('khushi', getdate());
