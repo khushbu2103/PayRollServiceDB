769c81e03f523978029102f41d3c1bf7663bdcea
khushbu <khushbusoni475@gmail.com>
2023-07-03 17:15:16 +0530

    [khushbu] modify fetch specific employee data and fetch all employee data based on date range

--- PayRollServiceDB.sql
+++ PayRollServiceDB.sql
@@ -17,4 +17,11 @@ insert into EmployeePayRoll (Name, StartDate)values('khushi', getdate());
 
 ---UC4 Fetch or Retrieve Data---
 select * from EmployeePayRoll
-select Name, Salary from EmployeePayRoll where Name='khushi'
+
+---Update Query---
+update EmployeePayRoll set StartDate='1996-05-06', Salary=65000 where Name='khushi'
+---UC5 fetch specific employee data and fetch all employee data based on date range---
+select * from EmployeePayRoll where Name='twiney'
+select * from EmployeePayRoll where StartDate between '2000-01-01' and getdate();
+---cast is an optional method---
+select * from EmployeePayRoll where StartDate between cast('2000-01-01' as date) and getdate();
